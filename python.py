916d402f030b0398b7473fe4e05297081817cd08
Georgii Gorbatckii <Georgii.Gorbatckii@edu.mayk.fi>
2025-03-13 09:19:13 +0200

    lisää nimen salailua

--- python.py
+++ python.py
@@ -5,11 +5,9 @@ def app(environ, respond):
 	respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
 	nimi = environ['PATH_INFO'].strip('/')
 	salanimi = nimi.replace('a', 'apa').replace('i', 'ipi') \
-        	.replace('n', 'non').replace('na', 'nana')
-	yield (f"Salainen nimesi on: <b>(salanimi.encode('utf-8')
-
-
-
+        	.replace('n', '<b>non</b>').replace('na', 'nana')
+	yield "<h1>huomio!</h1>".encode('utf-8')
+	yield ("<p>salainen <em>nimesi</em> on: %s</p>" % salanimi).encode('utf-8')
 
 
 if __name__ == '__main__':
